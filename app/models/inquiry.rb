cb296cfc1883e8a773b40d323f63886700d4c148
Ryo Fujiwara <jjcproject@icloud.com>
2020-11-23 19:40:52 +0900

    updated

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -4,9 +4,9 @@ class Inquiry
   attr_accessor :name, :company, :email, :phone, :message
 
   validates :name, :presence => {:message => '名前を入力してください'}
-  validates :company, :presence => {:company => '会社名を入力してください'}
+  validates :company, :presence => {:message => '会社名を入力してください'}
   validates :email, :presence => {:message => 'メールアドレスを入力してください'}
-  validates :phone, :presence => {:company => '電話番号を入力してください'},
+  validates :phone, :presence => {:message => '電話番号を入力してください'},
             :numericality => true,
             :length => { :minimum => 10, :maximum => 11}
 end
