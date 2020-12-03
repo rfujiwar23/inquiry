98a754a0fa633cbce792baf60bdd0229b4552959
Ryo Fujiwara <jjcproject@icloud.com>
2020-12-04 01:26:53 +0900

    stylizing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root  'inquiry#index'
+  get 'welcome/about'
+  get 'welcome/agricosmetica'
+  get 'welcome/index'
+  # root  'inquiry#index'
+  root 'welcome#index'
   get   'inquiry'         => 'inquiry#index'     # 入力画面
   post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
   post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
